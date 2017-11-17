dfc43264ec8e4d0fb3bab0c8ab41022e27ccde2a
Alejandro Madrigal <madrigalalex88@mail.fresnostate.edu>
2017-11-17 06:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var height = "74"
 module.exports = { name, height
 }
 if (name === "Susan"){
-  console.log(name);
+  console.log("name:" + " " +  name, "height:" + " " + height);
 }else {
   console.log("Expected the name to be Susan");
 }
